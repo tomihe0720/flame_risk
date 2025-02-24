78ca88d6b841bc5c9e92178e0fbc746a4c19e360
Yuhei Tomita <yuheitomita@Yuheis-MacBook-Air.local>
2025-02-24 09:06:09 +0900

    Deploy Next.js frontend to Vercel

--- node_modules/react-d3-cloud/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['./test/jest-setup.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/examples/', '/lib/'],
-  timers: 'fake',
-  resetModules: true,
-  resetMocks: true,
-};
